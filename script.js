d1785618ea05d48bcf09aeb6bf4722ef83799c12
Ha Duc Tuan <tuanhaduc13@gmail.com>
2026-02-21 00:05:39 +0700

    add ui

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 console.log("Welcome to the game!");
 
-function getComputerChoice(){
-    const min=Math.ceil(1);
-    const max=Math.floor(4);
-    const random = Math.floor(Math.random() * (max - min)+ min);//ramdom tu 0 den 1 * 0-3 + 1 thanh 1-4 lam tron thanh 1 2 3
-    if(random===1){
-        return "rock";
-    }else if(random===2){
-        return "paper";
-    }else{
-        return "scissors";
-    }
+// function getComputerChoice(){
+//     const min=Math.ceil(1);
+//     const max=Math.floor(4);
+//     const random = Math.floor(Math.random() * (max - min)+ min);//ramdom tu 0 den 1 * 0-3 + 1 thanh 1-4 lam tron thanh 1 2 3
+//     if(random===1){
+//         return "rock";
+//     }else if(random===2){
+//         return "paper";
+//     }else{
+//         return "scissors";
+//     }
+// }
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * 3)];
 }
 
 function getHumanChoice(){
@@ -60,29 +65,89 @@ function playRound (humanChoice,computerChoice) {
 }
 
 // playRound(humanSelection,computerSelection);
+// function playGame(){
+//     let humanScore = 0;
+//     let computerScore = 0;
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
+//     for (let i=0;i<99;i++){
+//         const humanSelection=getHumanChoice();
+//         const computerSelection= getComputerChoice();
+//         const result=playRound(humanSelection,computerSelection);
+//         if (result == "human"){
+//             humanScore++;
+//         }else if(result =="computer"){
+//             computerScore++;
+//         }
+//     }
+//     console.log("Human Score: "+ humanScore);
+//     console.log(`Computer Score: ${computerScore}`);
+//     if(humanScore>computerScore){
+//         console.log("You win, Congratz");
+//     }else if(humanScore<computerScore){
+//         console.log("You lose, too bad");
+//     }else if(humanScore==computerScore){
+//         console.log("it is a draw");
+//     }
+// }
 
-    for (let i=0;i<5;i++){
-        const humanSelection=getHumanChoice();
-        const computerSelection= getComputerChoice();
-        const result=playRound(humanSelection,computerSelection);
-        if (result == "human"){
-            humanScore++;
-        }else if(result =="computer"){
-            computerScore++;
-        }
+const rockBtn = document.createElement("button");
+rockBtn.textContent="Rock";
+const paperBtn = document.createElement("button");
+paperBtn.textContent="Paper";
+const scissorsBtn = document.createElement("button");
+scissorsBtn.textContent="Scissors";
+const playerSide= document.querySelector("#player-side");
+playerSide.appendChild(rockBtn);
+playerSide.appendChild(paperBtn);
+playerSide.appendChild(scissorsBtn);
+
+const resultDiv=document.querySelector("#score");
+
+const computerSide=document.querySelector("#computer-side")
+
+let humanScore=0;
+let computerScore=0;
+
+rockBtn.addEventListener("click",()=>{
+    const computerSelection= getComputerChoice();
+    const result = playRound("rock",computerSelection);
+    if (result == "human"){
+        humanScore++;
+    }else if(result =="computer"){
+        computerScore++;
     }
-    console.log("Human Score: "+ humanScore);
-    console.log(`Computer Score: ${computerScore}`);
-    if(humanScore>computerScore){
-        console.log("You win, Congratz");
-    }else if(humanScore<computerScore){
-        console.log("You lose, too bad");
-    }else if(humanScore==computerScore){
-        console.log("it is a draw");
+    Display(computerSelection, result);
+});
+
+paperBtn.addEventListener("click",()=>{
+    const computerSelection= getComputerChoice();
+    const result = playRound("paper",computerSelection);
+    if (result == "human"){
+        humanScore++;
+    }else if(result =="computer"){
+        computerScore++;
+    }
+    Display(computerSelection, result);
+});
+
+scissorsBtn.addEventListener("click",()=>{
+    const computerSelection= getComputerChoice();
+    const result = playRound("scissors",computerSelection);
+    if (result == "human"){
+        humanScore++;
+    }else if(result =="computer"){
+        computerScore++;
+    }
+    Display(computerSelection, result);
+});
+
+
+// playGame();
+function Display(computerChoice, result){
+    computerSide.innerHTML = `<p>Computer chose: <b>${computerChoice}</b></p>`;
+    resultDiv.innerHTML = `<p>Result: ${result}</p><p>Score: ${humanScore}-${computerScore}</p>`;
+    if(humanScore == 5 || computerScore == 5){
+        const winner=  humanScore ==5? "You win, Congratz!" : "You lose, sadly.";
+        resultDiv.innerHTML += `<p>${winner}</p>`;
     }
 }
-playGame();
